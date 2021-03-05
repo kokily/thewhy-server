e863cc93193087122a5a506b3afe25bc3164b4fe
kokily <kokily@naver.com>
2021-03-05 15:55:51 +0900

    APIs

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,32 @@
 import { ApolloServer } from 'apollo-server-koa';
-import Koa from 'koa';
+import Koa, { Context } from 'koa';
+import Router from 'koa-router';
+import cors from '@koa/cors';
+import bodyParser from 'koa-body';
 import schema from './libs/schema';
+import { devClient, isProd, prodClient } from './libs/constants';
 
 const app = new Koa();
+const router = new Router();
 
 const apollo = new ApolloServer({
   schema,
+  context: ({ ctx }: { ctx: Context }) => ({ ctx }),
 });
 
-apollo.applyMiddleware({ app });
+app.use(
+  cors({
+    origin: isProd ? prodClient : devClient,
+    credentials: true,
+  })
+);
+app.use(bodyParser({ multipart: true }));
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+router.get('/graphql', apollo.getMiddleware());
+router.post('/graphql', apollo.getMiddleware());
+
+apollo.applyMiddleware({ app, cors: false });
 
 export default app;
--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,1 +1,3 @@
 export const isProd = process.env.NODE_ENV === 'production';
+export const prodClient = 'https://thewhy.kr';
+export const devClient = 'http://localhost:3000';
